18a806fc5d107915100c7e1ded3b8ec531aa65fe
Bryan Triana <bryantrianarueda@gmail.com>
2017-07-29 22:17:09 -0500

    Add files via upload

---
+++ Resources/Files/BackgroundTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BackgroundTileset" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="../Textures/BackgroundTileset.png" width="512" height="512"/>
+</tileset>
