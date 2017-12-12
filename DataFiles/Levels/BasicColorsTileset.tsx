c22676468dc12573cd5ac5b273b9540d8ed615b5
Bryan Triana <bryantrianarueda@gmail.com>
2017-12-11 21:00:08 -0500

    Add files via upload

---
+++ DataFiles/Levels/BasicColorsTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BasicColors" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="../../Resources/Textures/BackgroundTileset.png" width="512" height="512"/>
+</tileset>
---
+++ DataFiles/Levels/Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset1" tilewidth="128" tileheight="128" tilecount="49" columns="7">
+ <image source="../../Resources/Textures/Tileset.png" width="896" height="896"/>
+</tileset>
